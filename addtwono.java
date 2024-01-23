2ce74841fdf013a6b9d0d2f35722576db5c09a69
aditikumari04 <157502459+aditikumari04@users.noreply.github.com>
2024-01-23 20:30:49 +0530

    Add files via upload

---
+++ addtwono.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class addtwono {
+    public static void main(String[] args) {
+        int a,b,c;
+        System.out.println("enter two no");
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println(c);
+    }
+}
