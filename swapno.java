7cf1c64c0217d188b62a6f3675b1e3edfa56ba77
aditikumari04 <157502459+aditikumari04@users.noreply.github.com>
2024-01-23 20:16:21 +0530

    Add files via upload

---
+++ swapno.java
@@ -0,0 +1,12 @@
+public class swapno {
+    public static void main(String[] args) {
+        int a,b,temp;
+        a=10;
+        b=20;
+        temp =a;
+        a=b;
+        b=temp;
+        System.out.println(a+""+b);
+        
+    }
+}
